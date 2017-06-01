8aa94414cdfe7ad29d36d84a6183bdfc78cd1184
Greg Joy <greg@growkudos.com>
2017-06-01 15:59:28 +0100

    Added tests for instance id retrieval function

--- main.go
+++ main.go
@@ -3,6 +3,11 @@ package main
 import (
 	"errors"
 	"fmt"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/autoscaling"
+	// "github.com/aws/aws-sdk-go/service/ec2"
 	"os"
 )
 
@@ -10,14 +15,52 @@ func main() {
 	fmt.Println("Checking Credentials...")
 
 	if ValidateAwsCredentials() != nil {
+		fmt.Println("Credentials are invalid!")
 		return
 	}
+
+	sess := session.Must(session.NewSession())
+
+	svc := autoscaling.New(sess)
+
+	// getAuotscalingGroupInstanceIDs (resp *autoscaling.DescribeAutoScalingGroupsInput)
+
+	params := &autoscaling.DescribeAutoScalingGroupsInput{
+		AutoScalingGroupNames: []*string{
+			aws.String("review-www"), // Required
+			// More values...
+		},
+		MaxRecords: aws.Int64(1),
+		// NextToken:  aws.String("XmlString"),
+	}
+
+	resp, err := svc.DescribeAutoScalingGroups(params)
+
+	if err != nil {
+		// TODO investigate https://golang.org/pkg/log/#Fatalf
+		fmt.Println("ERROR!")
+		fmt.Println(err.Error())
+		return
+	}
+
+	fmt.Printf("%v", resp)
+	// fmt.Printf("%v", resp.AutoScalingGroups[0].Instances[0])
 
 	fmt.Println("Everything is gravy!")
 }
 
 func AwsCredentials() string {
 	return "test"
+}
+
+func getAuotscalingGroupInstanceIDs(resp autoscaling.DescribeAutoScalingGroupsOutput) []string {
+	instanceIDs := []string{}
+
+	for _, instance := range resp.AutoScalingGroups[0].Instances {
+		instanceIDs = append(instanceIDs, *instance.InstanceId)
+	}
+
+	return instanceIDs
 }
 
 func ValidateAwsCredentials() error {
--- main_test.go
+++ main_test.go
@@ -1,15 +1,36 @@
 package main
 
 import (
-	"github.com/stretchr/testify/assert"
+	"fmt"
 	"os"
 	"testing"
+
+	"github.com/aws/aws-sdk-go/service/autoscaling"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestAwsCredentials(test *testing.T) {
 	credentials := AwsCredentials()
 
 	assert.Equal(test, credentials, "test", "This should fail")
+}
+
+func TestGetAuotscalingGroupInstanceIDs(test *testing.T) {
+	mockASGInstanceIds := []string{
+		"instanceIdOne",
+		"instanceIdTwo",
+		"instanceIdThree",
+		"instanceIdFour",
+		"instanceIdFive",
+	}
+
+	resp := getMockDescribeAutoScalingGroupsOutput(mockASGInstanceIds)
+
+	instanceIDs := getAuotscalingGroupInstanceIDs(resp)
+
+	for index, instanceID := range instanceIDs {
+		assert.Equal(test, instanceID, mockASGInstanceIds[index], nil)
+	}
 }
 
 func TestValidateAwsCredentialsValid(test *testing.T) {
@@ -30,4 +51,28 @@ func TestValidateAwsCredentialsAreMissing(test *testing.T) {
 	err := ValidateAwsCredentials()
 
 	assert.EqualError(test, err, "AWS credentials not set")
+}
+
+func getInstanceList(instanceIDs []string) []*autoscaling.Instance {
+	instances := []*autoscaling.Instance{}
+
+	for index := range instanceIDs {
+		instance := autoscaling.Instance{
+			InstanceId: &instanceIDs[index],
+		}
+
+		instances = append(instances, &instance)
+	}
+
+	return instances
+}
+
+func getMockDescribeAutoScalingGroupsOutput(instanceIds []string) autoscaling.DescribeAutoScalingGroupsOutput {
+	return autoscaling.DescribeAutoScalingGroupsOutput{
+		AutoScalingGroups: []*autoscaling.Group{
+			{
+				Instances: getInstanceList(instanceIds),
+			},
+		},
+	}
 }
